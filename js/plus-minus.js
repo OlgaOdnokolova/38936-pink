9da484523e9c8372a48a0aa5db98803382d35400
OlgaOdnokolova <inc.belko@gmail.com>
2015-10-26 23:00:34 +0200

    script +/-

---
+++ js/plus-minus.js
@@ -0,0 +1,41 @@
+
+var minusFt = document.querySelector(".fellow-travelers__remove")
+var plusFt = document.querySelector(".fellow-travelers__add")
+var peopleNumber = document.querySelector(".people-number")
+
+var minusTt = document.querySelector(".traveling-time__remove")
+var plusTt = document.querySelector(".traveling-time__add")
+var daysNumber = document.querySelector(".days-number")
+
+minusFt.addEventListener("click", function() {
+  if ((parseInt(peopleNumber.value, 10)) >= 1) {
+    peopleNumber.value = parseInt(peopleNumber.value, 10) - 1;
+  }
+})
+
+
+plusFt.addEventListener("click", function() {
+  if ((parseInt(peopleNumber.value, 10)) < 16) {
+    peopleNumber.value = parseInt(peopleNumber.value, 10) + 1;
+  }
+})
+
+
+minusTt.addEventListener("click", function() {
+  if ((parseInt(daysNumber.value, 10)) >= 1) {
+    daysNumber.value = parseInt(daysNumber.value, 10) - 1;
+  }
+})
+
+
+plusTt.addEventListener("click", function() {
+  if ((parseInt(daysNumber.value, 10)) < 30) {
+    daysNumber.value = parseInt(daysNumber.value, 10) + 1;
+  }
+})
+
+
+
+
+
+
